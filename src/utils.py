d082795a951cc00748b956b66dab80ee30aad6a9
kenneth-19p <69131027+kenneth-19p@users.noreply.github.com>
2025-03-07 11:49:07 -0500

    Hyperparameter and Tunning Implementation

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(obj, file_path):
@@ -23,7 +24,7 @@ def save_object(obj, file_path):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
    try:
     
     report = {}
@@ -31,8 +32,15 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
     for i in range(len(list(models))):
       
       model = list(models.values())[i]
+      para = params[list(models.keys())[i]]
 
-      model.fit(X_train, y_train)
+      gs = GridSearchCV(model,para,cv=5)
+      gs.fit(X_train,y_train)
+
+      model.set_params(**gs.best_params_)
+      model.fit(X_train,y_train)
+
+      #model.fit(X_train, y_train)
         
       y_train_pred = model.predict(X_train)
          
@@ -44,7 +52,7 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
       report[list(models.keys())[i]] = test_model_score
 
-      return report
+    return report
     
    except Exception as e:
       raise CustomException(e, sys)
